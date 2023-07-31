c509baca6e75da13ffddcce2bdbc628b13e48bdf
QingYunStudio <111875719+Buelie@users.noreply.github.com>
2023-07-31 22:59:49 +0800

    Create main.py

---
+++ Update/main.py
@@ -0,0 +1,34 @@
+import wget
+import os,sys
+import time
+
+import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.messagebox
+
+root = tk.Tk()
+
+def show():
+    for i in range(100):
+        # 每次更新加1
+        progressbarOne['value'] = i + 1
+        # 更新画面
+        root.update()
+        time.sleep(0.01)
+
+Label = ttk.Label(root,text="初始化中").pack()
+progressbarOne = tkinter.ttk.Progressbar(root)
+progressbarOne.pack(pady=20)
+progressbarOne['maximum'] = 100
+progressbarOne['value'] = 0
+progressbarOne['length'] = 200
+show()
+
+with open('cg.txt','r') as f:
+    path = f.read()
+
+os.remove(path+'\\MayDOS0.1_System.py')
+wget.download("https://buelie.github.io/MayDOS/main.py",path)
+os.rename("MayDOS0.1_System.py")
+
+root.mainloop()
