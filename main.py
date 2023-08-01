fce6af3cdae232b93b033323312103fdd48b6259
QingYunStudio <111875719+Buelie@users.noreply.github.com>
2023-08-01 23:09:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,9 @@ import tkinter as tk
 import tkinter.ttk as ttk
 import tkinter.messagebox
 
+os.system(r'title MayDOS')
+#os.system(r"mode con cols=1280 lines=648")
+
 if os.path.isdir('MayDOS_Login/') == False:
     os.makedirs('MayDOS_Login/')
 if os.path.isdir('important/') == False:
@@ -40,11 +43,6 @@ if os.path.isdir('important/log') == False:
     os.makedirs('important/log')
 if os.path.isdir('important/download') == False:
     os.makedirs('important/download')
-if os.path.isfile('important/download/cg.txt') == False:
-    path_url = os.getcwd() + "\\"
-    with open('important/download/cg.txt','w') as f:
-        f.write(path_url)
-        f.close()
 
 root = tk.Tk()
 
@@ -56,7 +54,7 @@ def show():
         root.update()
         time.sleep(0.01)
     Update = json.loads(requests.get("https://buelie.github.io/MayDOS/config.json").text)
-    code = "0.4.0"
+    code = "0.5.0"
     if Update["latest"]["default"] != code:
         Y_N_U = tkinter.messagebox.askyesno(title='更新提示',message=f'有可用更新，是否下载?\n当前版本:{code} -> {Update["latest"]["default"]}\n稍等一下，马上就好，在important/download/找到更新程序并运行即可')
         if Y_N_U == True:
@@ -64,7 +62,8 @@ def show():
                 os.remove("important/download/main.py")
             else:
                 pass
-            wget.download("https://buelie.github.io/MayDOS/Update/main.py","important/download")
+            wget.download("https://buelie.github.io/MayDOS/Update/update.py","")
+            os.system("START update.py")
             print("\n")
             quit()
         else:
@@ -83,6 +82,52 @@ show()
 
 init(autoreset=True)
 
+time_0 = 0.05
+
+print("MMMMMMMM               MMMMMMMM                                             DDDDDDDDDDDDD             OOOOOOOOO        SSSSSSSSSSSSSSS ")
+time.sleep(time_0)
+print("M:::::::M             M:::::::M                                             D::::::::::::DDD        OO:::::::::OO    SS:::::::::::::::S")
+time.sleep(time_0)
+print("M::::::::M           M::::::::M                                             D:::::::::::::::DD    OO:::::::::::::OO S:::::SSSSSS::::::S")
+time.sleep(time_0)
+print("M:::::::::M         M:::::::::M                                             DDD:::::DDDDD:::::D  O:::::::OOO:::::::OS:::::S     SSSSSSS")
+time.sleep(time_0)
+print("M::::::::::M       M::::::::::M  aaaaaaaaaaaaayyyyyyy           yyyyyyy       D:::::D    D:::::D O::::::O   O::::::OS:::::S            ")
+time.sleep(time_0)
+print("M:::::::::::M     M:::::::::::M  a::::::::::::ay:::::y         y:::::y        D:::::D     D:::::DO:::::O     O:::::OS:::::S            ")
+time.sleep(time_0)
+print("M:::::::M::::M   M::::M:::::::M  aaaaaaaaa:::::ay:::::y       y:::::y         D:::::D     D:::::DO:::::O     O:::::O S::::SSSS         ")
+time.sleep(time_0)
+print("M::::::M M::::M M::::M M::::::M           a::::a y:::::y     y:::::y          D:::::D     D:::::DO:::::O     O:::::O  SS::::::SSSSS    ")
+time.sleep(time_0)
+print("M::::::M  M::::M::::M  M::::::M    aaaaaaa:::::a  y:::::y   y:::::y           D:::::D     D:::::DO:::::O     O:::::O    SSS::::::::SS  ")
+time.sleep(time_0)
+print("M::::::M   M:::::::M   M::::::M  aa::::::::::::a   y:::::y y:::::y            D:::::D     D:::::DO:::::O     O:::::O       SSSSSS::::S ")
+time.sleep(time_0)
+print("M::::::M    M:::::M    M::::::M a::::aaaa::::::a    y:::::y:::::y             D:::::D     D:::::DO:::::O     O:::::O            S:::::S")
+time.sleep(time_0)
+print("M::::::M     MMMMM     M::::::Ma::::a    a:::::a     y:::::::::y              D:::::D    D:::::D O::::::O   O::::::O            S:::::S")
+time.sleep(time_0)
+print("M::::::M               M::::::Ma::::a    a:::::a      y:::::::y             DDD:::::DDDDD:::::D  O:::::::OOO:::::::OSSSSSSS     S:::::S")
+time.sleep(time_0)
+print("M::::::M               M::::::Ma:::::aaaa::::::a       y:::::y              D:::::::::::::::DD    OO:::::::::::::OO S::::::SSSSSS:::::S")
+time.sleep(time_0)
+print("M::::::M               M::::::M a::::::::::aa:::a     y:::::y               D::::::::::::DDD        OO:::::::::OO   S:::::::::::::::SS ")
+time.sleep(time_0)
+print("MMMMMMMM               MMMMMMMM  aaaaaaaaaa  aaaa    y:::::y                DDDDDDDDDDDDD             OOOOOOOOO      SSSSSSSSSSSSSSS   ")
+time.sleep(time_0)
+print("                                                    y:::::y                                                                            ")
+time.sleep(time_0)
+print("                                                   y:::::y                                                                             ")
+time.sleep(time_0)
+print("                                                  y:::::y                                                                              ")
+time.sleep(time_0)
+print("                                                 y:::::y                                                                               ")
+time.sleep(time_0)
+print("                                                yyyyyyy                                                                                ")
+time.sleep(time_0)
+print("================================================================ May DOS V0.4.0 ================================================================")
+
 def cls():
     os.system('cls')
 
@@ -91,7 +136,8 @@ error_account_file_not_found = False
 
 try:
     ver_open= open('important/Version.ver',mode='r')
-    Ver = ver_open.readline()
+    ver_open.seek(0, 0)
+    Ver = ver_open.read()
 except  FileNotFoundError:
     error_version_file_not_found = True
 
@@ -121,16 +167,16 @@ except FileNotFoundError:
     error_account_file_not_found = True
 
 if error_account_file_not_found == True and error_version_file_not_found == False:
-    print(Fore.RED + '未注册或账户文件异常丢失')
+    print(Fore.RED + '未注册或账户文件异常丢失,可尝试启动OOBE修复')
     input('按下回车键退出...')
     quit()
 elif error_version_file_not_found == True and error_account_file_not_found == False:
-    print(Fore.RED + '系统版本文件被移动或异常丢失，请尝试联系我们以修复')
+    print(Fore.RED + '系统版本文件被移动或异常丢失，请尝试联系我们以修复,可尝试启动OOBE修复')
     input('按下回车键退出...')
     quit()
 elif error_account_file_not_found  and error_version_file_not_found == True:
-    print(Fore.RED + '未找到系统版本文件及账户文件')
-    print(Fore.RED + '请确认是否注册并尝试联系我们')
+    print(Fore.RED + '未找到系统版本文件及账户文件,可尝试启动OOBE修复')
+    print(Fore.RED + '请确认是否注册并尝试联系我们,可尝试启动OOBE修复')
     input('按下回车键退出...')
     quit()
     
@@ -162,7 +208,7 @@ cls()
 
 print(Fore.GREEN + '正在准备你的MayDOS命令行......')
 print(Fore.GREEN + '请输入"usebook"以打开MayDOS0.1的使用手册和帮助')
-time.sleep(1.5)
+time.sleep(1)
 
 while True:
     cmd = input('MayDOS/Root>>>')
@@ -198,10 +244,19 @@ while True:
 
     elif cmd == 'cls':
         cls()
+    elif cmd == username:
+        JiTanLaiLuo = 0
+        while True:
+            if JiTanLaiLuo != 20 and JiTanLaiLuo < 20:
+                tkinter.messagebox.showerror(title="嗨嗨嗨",message="鸡汤来咯!")
+                JiTanLaiLuo += 1
+            elif JiTanLaiLuo >= 20:
+                tkinter.messagebox.showerror(title="嗨嗨嗨",message="鸡汤来咯!鸡汤来咯!鸡汤来咯!鸡汤来咯!")
+                JiTanLaiLuo += 1
 
     elif cmd =='sysver':
-        print(f'系统版本：MayDOS{Ver}')
-        print('开发：MayDOS开发团队 版权所有2023(C)')
+        print(f'系统版本：MayDOS {Ver}')
+        print('\n开发：MayDOS开发团队 版权所有2023(C)')
     else:
         print(Fore.YELLOW + "未定义的指令",cmd,Fore.YELLOW + "，请输入'usebook'以查看使用手册和帮助")
 
